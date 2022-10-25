7e96b2164dc6d8dcbb1a4a3de8add427fdf74ff2
wuyukwi <b1044763336@gmail.com>
2022-10-25 11:01:08 +0900

    10/25

--- engine/include/material.h
+++ engine/include/material.h
@@ -1,4 +1,4 @@
-/*********************************************************************************
+﻿/*********************************************************************************
 
   *FileName: material.h
             コウ  キガク
@@ -7,26 +7,41 @@
   *Date:  2022/04/02
 
 **********************************************************************************/
+#pragma once
+#include "math/Vector.h"
+#include "defines.h"
 
-#ifndef _MATERIAL_H_
-#define _MATERIAL_H_
-
-struct stMaterial
+struct Material
 {
-    stMaterial()
+    Material()
     {
-        emissiveR = emissiveG = emissiveB = emissiveA = 0.0f;
-        ambientR = ambientG = ambientB = ambientA = 1.0f;
-        diffuseR = diffuseG = diffuseB = diffuseA = 1.0f;
-        specularR = specularG = specularB = specularA = 0.0f;
+        Ambient = 1.0f;
+        Diffuse = 1.0f;
+        Specular = 0.0f;
+        Emissive = 0.0f;
         power = 0.0f;
     }
 
-    float emissiveR, emissiveG, emissiveB, emissiveA;
-    float ambientR, ambientG, ambientB, ambientA;
-    float diffuseR, diffuseG, diffuseB, diffuseA;
-    float specularR, specularG, specularB, specularA;
-    float power;
+    Material(Color ambient, Color diffuse, Color specular, Color emissive, float power)
+    {
+        this->Ambient = ambient;
+        this->Diffuse = diffuse;
+        this->Specular = specular;
+        this->Emissive = emissive;
+        this->power = power;
+    }
+
+
+    Color Diffuse; // マテリアルの拡散色 
+    Color Ambient; // マテリアルのアンビエント カラーを指定する値
+    Color Specular;// マテリアルの反射色を指定する値
+    Color Emissive; // マテリアルの放射色を指定する値
+    float power; // 反射ハイライトのシャープネスを指定する浮動小数点値。 値が大きいほど、ハイライトがシャープになります。
 };
 
-#endif // _MATERIAL_H_
+
+const Material WHITE_MTRL = Material(WHITE, WHITE, WHITE, BLACK, 2.0f);
+const Material RED_MTRL = Material(RED, RED, RED, BLACK, 2.0f);
+const Material GREEN_MTRL = Material(GREEN, GREEN, GREEN, BLACK, 2.0f);
+const Material BLUE_MTRL = Material(BLUE, BLUE, BLUE, BLACK, 2.0f);
+const Material YELLOW_MTRL = Material(YELLOW, YELLOW, YELLOW, BLACK, 2.0f);
